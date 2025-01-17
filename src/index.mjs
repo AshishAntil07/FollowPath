fdf2c3404636117fca382e61a8d991b68c66b46a
Ashish Antil <ashishantil.antil07@gmail.com>
2025-01-18 00:08:44 +0530

    Add features, fix bugs, remove demo
    Added:
    - timeline feature
    - single element support per instance
    - type declarations
    - stopping animation feature
    - partial iterations feature
    - fps tracking feature
    
    Fixed:
    - callback not called bug

--- src/index.mjs
+++ src/index.mjs
@@ -1,70 +1,144 @@
 class FollowPath {
 
+  #element;
+  #duration;
+  #stopAll;
+  #rotate;
+  #iterations;
+
+  /**
+   * Stores the key-value map of timeline
+   */
+  #timeline;
+
+  /**
+   * Stores the sorted keys of the timeline. **Sorted in Descending Order**
+   */
+  #timelineSorted;
+
   /**
    * Creates an instance.
-   * @param { HTMLElement[] } elements - Array of elements to animate
-   * @param { number } durations - Durations of animation(in ms) for each element in order
-   * @param { SVGPathElement | SVGPolylineElement } path - Path to follow
-   * @param { number } iterations - Number of times animation should be repeated
-   * @param { function } callback - Callback function to be called after animation
+   * 
+   * **Note: Animating multiple elements will be deprecated in future versions. Use multiple instances for multiple elements.**
+   * 
+   * @param { {
+   *  element: HTMLElement[] | HTMLElement,
+   *  duration: number[] | number,
+   *  path: SVGPathElement | SVGPolylineElement,
+   *  rotate: boolean,
+   *  iterations: number,
+   *  callback: function,
+   *  timeline: {
+   *    [key: string]: () => void
+   *  }
+   * } } config Configuration object
+   * - `element`: Element | Array of elements to animate.
+   * - `duration`: Duration of animation(in ms) for each element in order
+   * - `path`: Path to follow
+   * - `rotate`: Whether to rotate the elements along the path or not
+   * - `iterations`: Number of times animation should be repeated, can be set to `Infinity` for infinite loops, but that would cause the callback to never be called. You can also set it to a floating point number to do partial iterations.
+   * - `callback`: Callback function to be called after animation
+   * - `timeline`: Object containing functions to be called at specific points in the animation. The keys should be in the format of `percentage%` where percentage is the percentage of the path covered.
    */
 
-  constructor(elements, durations, path, iterations = 1, callback) {
-    this.elements = elements;
-    this.durations = durations;
+  constructor(config) {
+    const { element, duration, path, iterations, callback, rotate, timeline } = config;
+
+    this.#element = element;
+    this.#duration = duration;
     this.path = path;
-    this.iterations = iterations;
+    this.#iterations = iterations;
     this.callback = callback;
+    this.#rotate = rotate || false;
+    this.#timeline = timeline;
+    this.#timelineSorted = timeline?Object.keys(timeline).map(e => Number(e.slice(0, e.length-1))).sort((a, b) => b - a):null;
+    this.#stopAll = false;
   }
 
   /**
    * Animates the elements on the svg path in the given durations and calls the callback function after completion.
    * 
-   * You can specify an optional `iterNumber` (iteration number), to skip some iterations.
-   * @param { number } iterNumber 
-   * @returns 
+   * You can specify an optional `iterNumber` (iteration number), to skip some iterations, or pass it as a floating point number to start from between the path.
+   * @param { number } iterNumber Current Iteration Number
    */
-  animate(iterNumber = 1) {
-    if(!this.path){
-      throw Error('Runtime Error (FollowPath): Path Element not specified.');
+  animate(iterNumber = 0) {
+    if (!this.path || !this.#element || !this.#duration) {
+      throw Error(`Runtime Error (FollowPath): ${!this.path ? 'Path' : !this.#element ? 'Element' : !this.#duration ? 'Duration' : ''} not specified.`);
     }
 
+    this.fps = null;
+    let prevTime = null;
+
     const totalLength = this.path.getTotalLength();
-    if ((this.iterations <= 0 || iterNumber > this.iterations) && this.callback) {
+    if ((this.#iterations <= 0 || iterNumber > this.#iterations) && this.callback) {
+      this.fps = null;
       this.callback();
       return;
     }
-    const intervals = new Set();
-    this.elements.forEach((element, index) => {
-      let pixelsPerMs = totalLength / this.durations[index] * 10;
-      let currentPoint = 0;
-      const interval = setInterval(() => {
-        if (currentPoint >= totalLength) {
-          clearInterval(interval);
-          if (Math.max(...this.durations) === this.durations[index] && iterNumber <= this.iterations) {
-            this.animate(iterNumber)
-          }
+
+    const startPos = iterNumber - Math.floor(iterNumber);
+
+    let pixelsPerMs = (totalLength / this.#duration) * (1 / (this.fps || 60));
+    let currentPoint = totalLength * startPos;
+
+    const animator = () => {
+      // stop the animation if interrupted, or target iterations reached.
+      if (this.#stopAll || iterNumber >= this.#iterations) {
+        this.fps = null;
+        if (this.#stopAll) console.log('Animation Stopped by Interruption');
+        else if (this.callback) this.callback();
+        return;
+      };
+
+      // iteration completed
+      if (currentPoint >= totalLength) {
+        if (iterNumber <= this.#iterations) requestAnimationFrame(() => this.animate(iterNumber))
+          return;
+      }
+      
+      // calculate speed and fps.
+      if (prevTime) {
+        const timePerFrame = Date.now() - prevTime;
+        this.fps = 1000 / timePerFrame;
+        pixelsPerMs = (totalLength / this.#duration) * (timePerFrame);
+      }
+
+      // call timeline callback if present
+      if(this.#timeline && this.#timelineSorted){
+        while((iterNumber/this.#iterations)*100 >= this.#timelineSorted[this.#timelineSorted.length-1]) {
+          this.#timeline[this.#timelineSorted.pop()+'%']();
         }
+      }
+      
+      // calculating next step
+      let point = this.path.getPointAtLength(currentPoint);
+      let nextPoint = this.path.getPointAtLength(currentPoint + 1);
+      let angle = 0;
+      
+      if (!(nextPoint.x === this.path.getPointAtLength(totalLength).x && nextPoint.y === this.path.getPointAtLength(totalLength).y)) {
+        angle = Math.atan2(nextPoint.y - point.y, nextPoint.x - point.x) * 180 / Math.PI;
+      } else {
+        let prevPoint = this.path.getPointAtLength(currentPoint - 1);
+        angle = Math.atan2(point.y - prevPoint.y, point.x - prevPoint.x) * 180 / Math.PI;
+      }
+      
+      this.#element.style.transform = `translate(${point.x}px, ${point.y}px)${this.#rotate ? ` rotate(${angle}deg)` : ''}`;
+      currentPoint += pixelsPerMs;
+      iterNumber += pixelsPerMs / totalLength;
+      
+      prevTime = Date.now();
+      requestAnimationFrame(animator);
+    }
 
-        let point = this.path.getPointAtLength(currentPoint);
-        let nextPoint = this.path.getPointAtLength(currentPoint + 1);
-        let angle = 0;
+    prevTime = Date.now();
+    requestAnimationFrame(animator);
+  }
 
-        // getting the angle of rotation
-        if (!(nextPoint.x === this.path.getPointAtLength(totalLength).x && nextPoint.y === this.path.getPointAtLength(totalLength).y)) {
-          angle = Math.atan2(nextPoint.y - point.y, nextPoint.x - point.x) * 180 / Math.PI;
-        } else {
-          let prevPoint = this.path.getPointAtLength(currentPoint - 1);
-          angle = Math.atan2(point.y - prevPoint.y, point.x - prevPoint.x) * 180 / Math.PI;
-        }
-
-        element.style.transform = `translate(${point.x}px, ${point.y}px) rotate(${angle}deg)`;
-        currentPoint += pixelsPerMs;
-      }, 10);
-      intervals.add(interval);;
-    });
-
-    iterNumber++;
+  /**
+   * Stops the animation
+   */
+  stop() {
+    this.#stopAll = true;
   }
 }
 
